aae87db6f68fb8a37f69fdafd789e338cc722a8c
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-04-20 11:49:08 +0000

    Add allowed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-9bzcr)7%lud4y&m9ba+3@4iwyrbqf@&wgwv&q5=$b-pp86k2-u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ci-hello-django-dmccaffrey01.herokuapp.com']
 
 
 # Application definition
